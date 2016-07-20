3227b9f0573bd139008a175900c9a441f614795f
Thomas Lilly <tlilly3@gatech.edu>
2016-07-20 12:36:54 +0200

    updated database information

--- database.php
+++ database.php
@@ -1,10 +1,10 @@
 <?php
 
 #USE YOUR OWN CREDENTIALS
-$db_user = 'admin';
+$db_user = 'root';
 $db_password = 'b4rcA2016';
-$db_host = 'ec2-54-191-75-254.us-west-2.compute.amazonaws.com';
-$db_database = 'cs4400';
+$db_host = 'localhost';
+$db_database = 'travelreviews';
 
 $con =  mysqli_connect($db_host, $db_user, $db_password, $db_database);
 
