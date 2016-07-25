89ee076a78547a09ef44b47614f8125a575a3e40
Thomas Lilly <tlilly3@gatech.edu>
2016-07-25 23:46:57 +0200

    updated look of City Search

--- city_search.php
+++ city_search.php
@@ -8,21 +8,23 @@ session_start();
           href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
           integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7"
           crossorigin="anonymous"/>
-
+    <link rel = 'stylesheet' href = './css/city_search.css'/>
     <meta charset ='utf-8'/>
     <title>GTtravel</title>
   </head>
   <body>
-    <div class="container text-center">
+    <div class="container text-left">
       <div class='jumbotron'>
-        <h2>City Search</h2>
-          <form action = "" method="POST">
+        <h2><center>City Search</center></h2>
+          <form action = "" method="POST" role="form">
+            <div class="form-group">
             <?php
               $con = mysqli_connect($db_host, $db_user, $db_password, $db_database) or die("Connection Failed");
 
               $query_country = "SELECT * FROM Country";
               $result_country = mysqli_query($con, $query_country);
-              echo "<select name=\"country\" >";
+              echo "<label for=\"countrysel\">Country</label>";
+              echo "<select class=\"form-control\" id=\"countrysel\" name=\"country\" >";
               echo "<option value =\"empty\"></option>";
               while($row = mysqli_fetch_array($result_country)) {
                 echo "<option value = '" . $row['CountryName'] . "'>" . $row['CountryName'] . "</option>";
@@ -31,17 +33,22 @@ session_start();
 
               $query_city = "SELECT * FROM City";
               $result_city = mysqli_query($con, $query_city);
-              echo "<select name=\"cities\">";
+              echo "<label for=\"citysel\">City</label>";
+              echo "<select class=\"form-control\" id=\"citysel\" name=\"cities\">";
               echo "<option value =\"empty\"></option>";
               while($row = mysqli_fetch_array($result_city)) {
                 echo "<option value = '" . $row['CityName'] . "'>" . $row['CityName'] . "</option>";
               }
               echo "</select> <br />";
              ?>
-             <b>Population</b>
+             </div>
+             <div class="form-group">
+              <label for="pop">Population</label>
+        <!--      <b>Population</b> -->
              <!-- <input type="range" name="points" min="0" max="10"> -->
-             <input type="number" name="minimum" placeholder="Minimum"/> to
-             <input type="number" name="maximum" placeholder="Maximum"/><br />
+              <input type="number" class="form-control" id="pop" name="minimum" placeholder="Minimum"/> to
+              <input type="number" class="form-control" id="pop" name="maximum" placeholder="Maximum"/><br />
+            </div>
              <b class="text-center">City Languages</b>
              <?php
                 $con = mysqli_connect($db_host, $db_user, $db_password, $db_database) or die("Connection Failed");
@@ -52,6 +59,7 @@ session_start();
                     echo "<input type = \"radio\" name=\"language\" value = '" . $row['LanguageName'] . "'>" .  $row['LanguageName'] . "</input><br />";
                 }
               ?>
+
               <input type="submit" name="submit" value="Search">
           </form>
           <?php
