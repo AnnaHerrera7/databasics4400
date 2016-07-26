82f71dae59e7f234fc5d9bc3ba39bcd7b3422e79
michaelchen523 <michaelchen523@gmail.com>
2016-07-27 01:55:35 +0200

    Review link works

--- home.php
+++ home.php
@@ -6,6 +6,10 @@ session_start();
 <html>
   <head>
 
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+    
     <link rel="stylesheet"
           href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
           integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7"
@@ -16,7 +20,6 @@ session_start();
           crossorigin="anonymous">
 
     <link rel = 'stylesheet' href = './css/home.css'/>
-
 
     <meta charset ='utf-8'/>
     <title>GTtravel</title>
