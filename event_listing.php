49e633504e0c044caea3bfc6b936572ac840a217
michaelchen523 <michaelchen523@gmail.com>
2016-07-27 02:09:12 +0200

    navbar

--- event_listing.php
+++ event_listing.php
@@ -4,30 +4,19 @@ session_start();
 ?>
 <html>
   <head>
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+    
     <link rel="stylesheet"
           href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
           integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7"
           crossorigin="anonymous"/>
 
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"
-          rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1"
-          crossorigin="anonymous">
-
-    <link rel = 'stylesheet' href = './css/event_listing.css'/>
-
     <meta charset ='utf-8'/>
     <title>GTtravel</title>
   </head>
   <body>
-    <nav class = 'navbar navbar-light navbar-fixed-top'>
-        <div id = "spy-scroll-id" class = 'container'>
-          <ul class="nav navbar-nav navbar-right">
-            <li class = 'active'><a href="home.php"><i class="fa fa-home"></i>Home</a></li>
-            <li><a href = "login.php"><i class ="fa fa-user"></i>Logout</a></li>
-          </ul>
-          <a href = '#' class = "pull-left navbar-left"><img id = "logo" src = "./images/LogoMakr.png"></a>
-        </div>
-    </nav>
     <div class="container text-center">
       <div class = "jumbotron">
         <?php
@@ -35,23 +24,20 @@ session_start();
         ini_set("display_errors", 1);
         $con = mysqli_connect($db_host, $db_user, $db_password, $db_database) or die("Connection Failed");
         $event = $_GET['a'];
-        $address = $_GET['b'];
-        $city = $_GET['c'];
-        $country = $_GET['d'];
-        $date = $_GET['e'];
-        $startTime = $_GET['f'];
         echo "<h2>" . $event . "</h2>";
 
-        $query = "SELECT Event.EndTime, Event.Cost, Event.EventType, Event.StudentDiscount, Event.Description, AVG(Score) as AvgScore
-                  FROM Event, Review RIGHT OUTER JOIN Reviewable ON Review.ReviewableID=Reviewable.ReviewableID
-                  WHERE Event.Ename = \"$event\" AND Event.Address = \"$address\"
-                  AND Event.CityName = \"$city\" AND Event.CountryName = \"$country\"
-                  AND Event.EDate = \"$date\" AND Event.StartTime = \"$startTime\"
-                  AND Event.ReviewableID = Reviewable.ReviewableID;";
+        $query = "SELECT *
+                  FROM Event
+                  WHERE Ename = \"$event\";";
 
         $result = mysqli_query($con, $query);
         $result_array = mysqli_fetch_array($result);
+        $address = $result_array['Address'];
+        $city = $result_array['CityName'];
+        $country = $result_array['CountryName'];
         $location = "<h4>Location: $address $city, $country</h4>";
+        $date = $result_array['EDate'];
+        $startTime = $result_array['StartTime'];
         $endTime = $result_array['EndTime'];
         $dateTime = "<h4>Date & Time: $date at $startTime - $endTime";
         $cost = $result_array['Cost'];
@@ -63,25 +49,13 @@ session_start();
         } else {
             $discount_string = "Yes";
         }
-        $avg = $result_array['AvgScore'];
-          if($avg == NULL) {
-            $avgs = "No Reviews Yet";
-          } else {
-            $avgs = $avg;
-          }
         echo $location;
         echo $dateTime;
         echo "<h4>Cost: $cost Euros</h4>";
         echo "<h4>Category: $category";
-
-        echo "<h4>Discount: $discount_string</h4>";
-        echo "<h4>Average Review Score: $avgs</h4>";
-        echo "<h4 id = \"DescripTitle\">Description: </h4>";
-        echo "<div class = \"container text-center\" id = \"Descrip\">";
-        echo "<h4>$description</h4>";
-        echo "</div>";
-        echo "<br/>";
-        echo "<h3>Reviews:</h3>";
+        echo "<h4>Discount: $discount_string</4>";
+        echo "<h4>Description: $description</h4>";
+        echo "Reviews:";
         $query_reviews = "SELECT Review.Username, Review.RDate, Review.Score, Review.Description
                           FROM Event, Review, Reviewable
                           WHERE Event.CityName = \"$city\" AND Event.CountryName = \"$country\" AND Event.Address = \"$address\"
@@ -90,8 +64,8 @@ session_start();
                           ORDER BY Review.RDate DESC;";
 
         $result_reviews = mysqli_query($con, $query_reviews);
-        if(mysqli_num_rows($result_reviews) > -1) {
-          echo "<table class= \"table table-striped\" border=\"1\">";
+        if(mysqli_num_rows($result_reviews) > 0) {
+          echo "<table class= \"text-center\" border=\"1\">";
           echo "<tr>";
               echo "<th>Username</th><th>Date</th><th>Score</th><th>Description</th>";
           echo "</tr>";
